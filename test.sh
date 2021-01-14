b821258c93c72a0812ea779e24e90eeea748fc82
Manuel Bertele <manuel.bertele2003@gmail.com>
2021-01-14 22:15:47 +0100

    Updated test.sh
    Signed-off-by: Manuel Bertele <manuel.bertele2003@gmail.com>

--- test.sh
+++ test.sh
@@ -3,65 +3,41 @@
 executabledir="./output/"
 executable="main"
 
-#ATTENTION WHEN CHECKING FOR STRING CONVERSION A EXTRA SPACE IS REQUIRED AT THE END OF THE EXPECTED OUTPUT SEE LINE 55 AND 60
+parameters=(
+	"-i 16 -n 0 -o 2"
+	"-i 16 -n f16 -o 2"
+	"-i 16 -n f16 -o 10"
+	"-i 2 -n 1010 -o 2"
+	"-i 2 -n 1010 -o 10"
+	"-i 2 -n 1010 -o 16"
+	"-i 10 -n 432 -o 2"
+	"-i 10 -n 421 -o 32"
+	"-i 10 -n 55 -o 10"
+	"-i 5 -n 423 -o 13"
+	"-s Hello -o 2"
+	"-s HelloMyFriend -o 10"
+)
 
-parameters="-i 16 -n 0 -o 2"
-expected="0"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
+expectations=(
+	"0"
+	"111100010110"
+	"3862"
+	"1010"
+	"10"
+	"A"
+	"110110000"
+	"D5"
+	"55"
+	"89"
+	"1001000 1100101 1101100 1101100 1101111 "
+	"72 101 108 108 111 77 121 70 114 105 101 110 100 "
+)
 
-parameters="-i 16 -n f16 -o 2"
-expected="111100010110"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
+length=${#parameters[@]}
 
-#
-parameters="-i 16 -n f16 -o 10"
-expected="3862"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
+for (( i = 0; i < $length; i++ ))
+do
+	output="$($executabledir$executable ${parameters[$i]})"
+	[ "$output" = "${expectations[$i]}" ] && echo -e "OK\t$executabledir$executable ${parameters[$i]}" || echo -e "FAILED\t$executabledir$executable ${parameters[$i]} expected ${expect[$i]} but got $output"
 
-parameters="-i 2 -n 1010 -o 2"
-expected="1010"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-i 2 -n 1010 -o 10"
-expected="10"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-i 2 -n 1010 -o 16"
-expected="A"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-i 10 -n 432 -o 2"
-expected="110110000"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-i 10 -n 421 -o 32"
-expected="D5"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-i 10 -n 55 -o 10"
-expected="55"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-i 5 -n 423 -o 13"
-expected="89"
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-s Hello -o 2"
-expected="1001000 1100101 1101100 1101100 1101111 "
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
-
-parameters="-s HelloMyFriend -o 10"
-expected="72 101 108 108 111 77 121 70 114 105 101 110 100 "
-output="$($executabledir$executable $parameters)"
-[ "$output" = "$expected" ] && printf "OK\t$executabledir$executable $parameters\n" || printf "FAILED\t$executabledir$executable $parameters expected $expected but got $output\n"
+done
